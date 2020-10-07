79c5bbd067d3c8a47239308609bf54c7a01c5bed
pitchounvivi <pitchounvivi@gmail.com>
2020-10-07 15:21:53 +0200

    création et ajout constante dans Entreprise.java

---
+++ src/main/java/com/ipiecoles/java/java220/Entreprise.java
@@ -0,0 +1,9 @@
+package com.ipiecoles.java.java220;
+
+public class Entreprise {
+    public static final Double SALAIRE_BASE = 1480.27;
+    public static final Integer NB_CONGES_BASE = 25;
+    public static final Double INDICE_MANAGER = 1.3;
+    public static final Double PRIME_MANAGER_PAR_TECHNICIEN = 250d; //ou 250.0
+    public static final Double PRIME_ANCIENNETE = 100d; // ou 100.0
+}
