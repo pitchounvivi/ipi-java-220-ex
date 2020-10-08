a45f45a4eb3131c2ff3872268ece9b9242fe48ed
pitchounvivi <pitchounvivi@gmail.com>
2020-10-08 14:37:23 +0200

    ajout attribut grade + getter/setter + constructeur dans Technicien.java

--- src/main/java/com/ipiecoles/java/java220/Technicien.java
+++ src/main/java/com/ipiecoles/java/java220/Technicien.java
@@ -4,10 +4,25 @@ import org.joda.time.LocalDate;
 
 public class Technicien extends Employe{
 
+    private Integer grade;
+
     public Technicien() {
     }
 
     public Technicien(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire) {
         super(nom, prenom, matricule, dateEmbauche, salaire);
+    }
+
+    public Integer getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Integer grade) {
+        this.grade = grade;
+    }
+
+    public Technicien(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire, Integer grade) {
+        super(nom, prenom, matricule, dateEmbauche, salaire);
+        this.grade = grade;
     }
 }
