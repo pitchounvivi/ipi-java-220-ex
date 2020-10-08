4deba35b8fbf767e470a43ad8b05b2534e32bf0f
pitchounvivi <pitchounvivi@gmail.com>
2020-10-08 11:40:43 +0200

    Création du constructeur (readme 204) dans Employe.java

--- src/main/java/com/ipiecoles/java/java220/Commercial.java
+++ src/main/java/com/ipiecoles/java/java220/Commercial.java
@@ -1,5 +1,7 @@
 package com.ipiecoles.java.java220;
 
+import org.joda.time.LocalDate;
+
 public class Commercial extends Employe{
     private Double caAnnuel;
 
@@ -8,6 +10,24 @@ public class Commercial extends Employe{
     }
 
     public void setCaAnnuel(Double caAnnuel) {
+        this.caAnnuel = caAnnuel;
+    }
+
+    /*//@Override
+    public Double getPrimeAnnuelle(Double caAnnuel) {
+
+        Double prime = (caAnnuel*5/100);
+
+        if (prime < 500){
+            return prime = 500d;
+        }
+        else{
+            return prime;
+        }
+    }*/
+
+    public Commercial(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire, Double caAnnuel) {
+        super(nom, prenom, matricule, dateEmbauche, salaire);
         this.caAnnuel = caAnnuel;
     }
 }
