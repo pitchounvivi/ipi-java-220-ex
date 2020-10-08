1acf2cd971f00266999cb33bc27998c74de924d9
pitchounvivi <pitchounvivi@gmail.com>
2020-10-08 10:52:11 +0200

    ajout test méthode toString dans Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,11 @@ import com.ipiecoles.java.java220.Employe;
 import org.joda.time.LocalDate;
 
 public class Main {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello World");
 
-            Employe employe = new Employe("a","b","1", LocalDate.now(),500.0);
-            System.out.println(employe.augmenterSalaire(0.5));
+        Employe employe = new Employe("a","b","1", LocalDate.now(),500.0);
+        System.out.println(employe.augmenterSalaire(0.5));
+        System.out.println(employe.toString());
     }
 }
