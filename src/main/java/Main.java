ca3f76e0bfbb29c7ceeec2d49909b05ac2e19253
pitchounvivi <pitchounvivi@gmail.com>
2020-10-08 10:20:33 +0200

    test de la méthode dans Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
+import com.ipiecoles.java.java220.Employe;
+import org.joda.time.LocalDate;
+
 public class Main {
         public static void main(String[] args) {
         System.out.println("Hello World");
+
+            Employe employe = new Employe("a","b","1", LocalDate.now(),500.0);
+            System.out.println(employe.augmenterSalaire(0.5));
     }
 }
